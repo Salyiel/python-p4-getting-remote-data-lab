db48a5d8e4782dcc0822718a7eb23314e7d1c774
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-08-27 15:47:32 +0300

    Complete assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content  # this returns the body as bytes
         pass
 
     def load_json(self):
+        # Get the response body and convert it to a Python object (list or dict)
+        response_body = self.get_response_body()
+        return json.loads(response_body.decode('utf-8'))
         pass
